3531286c5e13c24b84efb1f0383c8c434a119f93
Judith Rancourt <judith.rancourt@gmail.com>
2022-02-16 18:59:08 -0500

    Ajout projet

---
+++ assets/scripts/depanneur.js
@@ -0,0 +1,346 @@
+let depanneur = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Dépanneur La ressource"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.44846367835999,
+            45.59870510025369
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Les Assoiffés - Boutique"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.51049348777372,
+            45.501645923105315
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Le p’tit verrat"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.81469018857145,
+            45.75974586431458
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Dépanneur Peluso"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.56320646343423,
+            45.537113034127024 
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Le Monde des Bières"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.5766059865446,
+            45.53040882397745
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Expérience Bière"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.56076498859684,
+            45.51952722997329
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Le Frigo de Bacchus"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.57435673085938,
+            45.55020532337965
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Broue Pub Brouhaha Rosemont"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.5876555750855,
+            45.54425596324378
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Broue Pub Brouhaha Ahuntsic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.65575765883729,
+            45.57821661916144
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Dépanneur chez Gibb"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.12621995706334,
+            48.24626886634679
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "L'Axe du Malt"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -71.2200831173281,
+            46.81075070955137
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Le Parti Bière"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.56763820202181,
+            45.59820631546651
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Dépanneur du Lac"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.02584553084759,
+            45.93028633583538
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Épicerie Prette"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.82318630028055,
+            45.43010375258191
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Veux-tu une bière?"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.63585051194995,
+            45.54513209014008
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Le Bièrologue"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.53988783096085,
+            45.55282854983928
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "La Grange à Houblon"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.08719148676828,
+            46.038630671189665
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Bières et Saveurs du Terroir"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -71.95429114438808,
+            46.05999697834594
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Bières Dépôt Au Vent du Nord"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -71.90065358978676,
+            45.406841604777206
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Maltéhops"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.56774283091433,
+            45.45720688959009
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "La Cave à Bière"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.52329400207488,
+            45.57295426473588
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Marché Centre-ville Chicoutimi"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -71.0696626731482,
+            48.427067844179355
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Le Frigo des Dieux"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.51141745974418,
+            45.64989107662634
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "Espace Houblon"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.61375887825008,
+            45.74324505805111 
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "La Glacière de l’Est"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.52953590022325,
+            45.60434865317408
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "nom": "La Barik"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.58616359649828,
+            46.35720736979316
+          ]
+        }
+      }
+    ]
+  }
+
+ 
---
+++ assets/scripts/main.js
@@ -0,0 +1,141 @@
+const maCarte = L.map('map', {attributionControl: false}).setView([45.6, -73.6], 10);
+
+// *** ICONES ***
+
+const depIcone = L.icon({
+    iconUrl: 'assets/img/canette.png',
+    iconSize: [25, 25],
+    iconAnchor: [0, 0],
+    popupAnchor: [15, -5]
+});
+
+const microIcone = L.icon({
+    iconUrl: 'assets/img/choppe.png',
+    iconSize: [25, 25],
+    iconAnchor: [0, 0],
+    popupAnchor: [15, -5]
+});
+
+
+const personneIcone = L.icon({
+    iconUrl: 'assets/img/person.png',
+    iconSize: [35, 35],
+    iconAnchor: [0, 0],
+    popupAnchor: [15, -5]
+});
+
+const apikey = 'AAPK2a71336bee2b4dab8c8dbc79b208b359QrvXlwSkwJ3F7cG-Zw2D_f0TwFK0M-GoBa8Bvk55N3cmMkSAIhbJQRzzhovtdfM_';
+
+// *** FOND DE COUCHES ***
+
+const fondCouleur = L.esri.Vector.vectorBasemapLayer('ArcGIS:ColoredPencil', {
+    apikey: apikey
+});
+
+const fondGris = L.esri.Vector.vectorBasemapLayer('ArcGIS:Newspaper', {
+    apikey: apikey
+});
+
+const fondNavigation = L.esri.Vector.vectorBasemapLayer('ArcGIS:Navigation', {
+	apikey: apikey
+});
+
+fondCouleur.addTo(maCarte)
+
+// *** COUCHES DÉPANNEURS BIÈRES ***
+
+let depanneurs = L.geoJSON(depanneur, {
+    pointToLayer: function(geoJsonPoint, latlng) {
+        return L.marker(latlng, {icon: depIcone});
+    }
+}).bindPopup(function (layer) {
+    return `<h3>${layer.feature.properties.nom}</h3>`
+}).addTo(maCarte);
+
+// *** COUCHE DES MICROBRASSERIES DU QUÉBEC *** 
+
+const urlMicro = 'https://services6.arcgis.com/pG4MNR4EC4WmfCRT/ArcGIS/rest/services/MicroBrasserie/FeatureServer/0';
+
+let micro = 
+L.esri.featureLayer({
+    url:  urlMicro,
+    pointToLayer: function (geojson, latlng) {
+      return L.marker(latlng, {
+        icon: microIcone
+      });
+    }
+}).bindPopup(function (layer) {
+    return `<h3>${layer.feature.properties.Nom__raison_sociale_}</h3> 
+            
+            <p>${layer.feature.properties.Adresse}</p>
+            <a href="${layer.feature.properties.Site_Web}" target="_blank">${layer.feature.properties.Site_Web}</a>`;
+   
+}).addTo(maCarte);
+
+// *** CONTROLE DE COUCHES ***
+
+let baseLayers = {
+    'Crayons de couleur': fondCouleur,
+    'Gris': fondGris,
+    'Navigation': fondNavigation
+}
+
+let overlays = {
+    'Dépanneurs': depanneurs,
+    'Microbrasserie': micro
+}
+
+L.control.layers(baseLayers, overlays).addTo(maCarte);
+
+// **GÉOLOCALISATION**
+
+const geoLocButton = document.getElementById('activerGeolocalisation');
+const pPosition = document.getElementById('affichePosition');
+
+geoLocButton.addEventListener('click', getPosition);
+
+function getPosition() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(affichePosition, afficheErreur);
+    } else {
+        pPosition.innerHTML = "Il semble y avoir un problème 😿"
+    }
+}
+
+function affichePosition(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+    pPosition.innerHTML = "Lat :" + lat + "<br> Long : " + long;
+    L.marker([lat, long], {icon: personneIcone}).addTo(maCarte)
+    .bindPopup('Vous êtes ici!')
+    .openPopup();
+}
+
+function afficheErreur(erreur) {
+    pPosition.innerHTML = "Problème!";
+}
+
+// *** BARRE D'ÉCHECHELLE ***
+
+let echelle = L.control
+    .scale({'imperial': false})
+    .addTo(maCarte);
+
+// *** BARRE DE RECHERCHE ***
+
+const searchControl = L.esri.Geocoding.geosearch({
+	position: 'topright',
+	placeholder: 'Rechercher...',
+	useMapBounds: false,
+	providers: [
+		L.esri.Geocoding.arcgisOnlineProvider({
+			apikey: apikey,
+			nearby: {
+				lat: 46,
+				lng: -71,
+			},
+		}),
+	],
+}).addTo(maCarte);
+
